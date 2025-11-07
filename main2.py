62fadddcb6de2fdab88e8598fd1e3d0a9444179b
artemmadzara1 <artemmadzara@gmail.com>
2025-11-07 22:46:32 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,40 @@
+# def  task_1(n,b=0):
+#     if b ==n:
+#         print(b)
+#         return n
+#     else:
+#         if b == n:
+#             print(b)
+#             return n
+#         print(b)
+#     return task_1(n, b +1)
+# task_1(7)
+# def task2(a, b):
+#     if a == b:
+#         print(a)
+#         return a
+#     elif a > b:
+#         print(b)
+#         return task2(a, b + 1)
+#     else:
+#         print(a)
+#         return task2(a + 1, b)
+#
+# # print(task2(a=int(input()), b=int(input())))
+#4
+# def fib(n):
+#     if n == 1:
+#         return 0
+#     if n ==2:
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
+# print(fib(n=int(input())))
+# def task_3(n):
+#     if n==1:
+#         print("yes")
+#     elif n%2!=0 or n<1:
+#         print("no")
+#     else:
+#         task_3(n//2)
+# task_3(int(input()))
