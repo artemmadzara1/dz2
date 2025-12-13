44219d980ad20557f470409a18e155e44c735d25
artemmadzara1 <artemmadzara@gmail.com>
2025-12-13 15:17:24 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,106 @@
-n=int(input())
-k26=0
-k13=0
-k2=0
-for i in range(n):
-    x =int(input())
-    if x%26==0:
-        k26+=1
-    elif x %13==0:
-        k13+=1
-    elif x%2==0:
-        k2+=1
-result=k26*(n-k26)+k26*(k26-1)//2+k2*k13
-print(result)
+import time
 
 
-n = int(input())
-k62=0
-k31=0
-k2=0
-for i in range(n):
-    x =int(input())
-    if x %62 ==0:
-        k62+=1
-    elif x %31==0:
-        k31+=1
-    elif x %2==0:
-        k2+=1
-result=k62*(n-k62)+k62*(k62-1)//2+k2*k31
-print(result)
+def task1():
+    z = 0
+    N = 20
+    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
+    print(result)
+    for i in range(len(result) - 1):
+        if result[i] + result[i + 1] >= 0:
+            z += 1
+    print(f"кол пар элем чья сумма полож {z}")
 
+
+tick = time.perf_counter()
+task1()
+tock = time.perf_counter()
+print(f"вычисление заняло {tock-tick:0.8f}")
+
+
+def task2():
+    N = 20
+    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
+    print(result)
+    z = result[0] + result[1]
+    for i in range(len(result)-1):
+        sum = result[i] + result[i+1]
+        if sum > z:
+            z = sum
+    print(f"макс сумма {z}")
+
+
+tick = time.perf_counter()
+task2()
+tock = time.perf_counter()
+print(f"вычисление заняло {tock-tick:0.8f}")
+
+
+def task3():
+    N = 20
+    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
+    print(result)
+    for i in range(len(result) - 1):
+        z = result[i]
+        d = result[i + 1]
+        if z > d:
+            print(z)
+        else:
+            print(d)
+
+
+tick = time.perf_counter()
+task3()
+tock = time.perf_counter()
+print(f"вычисление заняло {tock-tick:0.8f}")
+
+
+def task4():
+    N = 20
+    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
+    print(result)
+    z = 0
+    for i in range(len(result) - 2):
+        if result[i] % 2 == 0 and result[i + 1] % 2 == 0 and result[i + 2] % 2 == 0:
+            z += 1
+    print(f"тройки элем {z}")
+
+
+tick = time.perf_counter()
+task4()
+tock = time.perf_counter()
+print(f"вычисление заняло {tock-tick:0.8f}")
+
+
+def task5():
+    N = 20
+    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
+    print(result)
+    z = 0
+    for i in range(len(result) - 1):
+        if result[i] * result[i + 1] % 7 == 2:
+            z += 1
+    print(z)
+
+
+tick = time.perf_counter()
+task5()
+tock = time.perf_counter()
+print(f"вычисление заняло {tock-tick:0.8f}")
+
+
+def task6():
+    N = 20
+    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
+    print(result)
+    z = 0
+    for i in range(len(result)-1):
+        if result[i] - result[i + 1] > abs(14) and result[i] / result[i+1] > abs(1):
+            z += 1
+    print(z)
+
+
+tick = time.perf_counter()
+task6()
+tock = time.perf_counter()
+print(f"вычисление заняло {tock-tick:0.8f}")
