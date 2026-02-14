757ee1722b8abb315267f0a7cbb5b1a959463ce8
artemmadzara1 <artemmadzara@gmail.com>
2026-02-14 12:04:02 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,102 +1,20 @@
-import time
-
-
-def task1():
-    z = 0
-    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
-    print(result)
-    for i in range(len(result) - 1):
-        if result[i] + result[i + 1] >= 0:
-            z += 1
-    print(f"кол пар элем чья сумма полож {z}")
-
-
-tick = time.perf_counter()
-task1()
-tock = time.perf_counter()
-print(f"вычисление заняло {tock-tick:0.8f}")
-
-
-def task2():
-    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
-    print(result)
-    z = result[0] + result[1]
-    for i in range(len(result)-1):
-        summ = result[i] + result[i+1]
-        if summ > z:
-            z = summ
-    print(f"макс сумма {z}")
-
-
-tick = time.perf_counter()
-task2()
-tock = time.perf_counter()
-print(f"вычисление заняло {tock-tick:0.8f}")
-
-
-def task3():
-    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
-    print(result)
-    for i in range(len(result) - 1):
-        z = result[i]
-        d = result[i + 1]
-        if z > d:
-            print(z)
-        else:
-            print(d)
-
-
-tick = time.perf_counter()
-task3()
-tock = time.perf_counter()
-print(f"вычисление заняло {tock-tick:0.8f}")
-
-
-def task4():
-    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
-    print(result)
-    z = 0
-    for i in range(len(result) - 2):
-        if result[i] % 2 == 0 and result[i + 1] % 2 == 0 and result[i + 2] % 2 == 0:
-            z += 1
-    print(f"тройки элем {z}")
-
-
-tick = time.perf_counter()
-task4()
-tock = time.perf_counter()
-print(f"вычисление заняло {tock-tick:0.8f}")
-
-
-def task5():
-    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
-    print(result)
-    z = 0
-    for i in range(len(result) - 1):
-        if result[i] * result[i + 1] % 7 == 2:
-            z += 1
-    print(z)
-
-
-tick = time.perf_counter()
-task5()
-tock = time.perf_counter()
-print(f"вычисление заняло {tock-tick:0.8f}")
-
-
-def task6():
-    result = [584, 902, 304, 906, 806, 328, 92, 100, 481, 422, 361, 691, 383, 922, 408, 235, 226, 15, 251, 487]
-    print(result)
-    z = 0
-    for i in range(len(result)-1):
-        if result[i] - result[i + 1] > abs(14) and result[i] / result[i+1] > abs(1):
-            z += 1
-    print(z)
-
-
-tick = time.perf_counter()
-task6()
-tock = time.perf_counter()
-
-print(f"вычисление заняло {tock-tick:0.8f}")
+# a =input("введите 9 букв")
+# z=1
+# for i in range(1,len(a)+1):
+#     z=z*i
+# print(z)
+# a=10
+# b=12
+# for i in range(1,3):
+#     a=a*(a-1)
+# for j in range(1):
+#     b=b*(b-1)
+# z=a+b
+# print(z)
+#
+# count = 0
+# for n in range(100000, 1000000):
+#     if sum(int(i) for i in str(n)) == 30:
+#         count += 1
+# print(count)
 
